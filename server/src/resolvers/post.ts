174bc2856ba905c16ae4e312b3f7543c6491b6db
PixelQuasar <48384867+PixelQuasar@users.noreply.github.com>
2021-03-07 22:53:08 +0100

    add Int type to args for post

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,5 +1,5 @@
 import { Post } from '../entities/Post';
-import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
 import { MyContext } from '../types';
 
 @Resolver()
@@ -11,7 +11,7 @@ export class PostResolver {
 
     @Query(() => Post, { nullable: true })
     post(
-        @Arg('id') id: number,
+        @Arg('id', () => Int) id: number,
         @Ctx() { em }: MyContext,
     ): Promise<Post | null> {
         return em.findOne(Post, { id });
